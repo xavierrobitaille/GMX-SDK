c31975f7365ecb0a11e606a78bdae8a5976405bf
Xavier Robitaille <xavier@featherfinance.com>
2024-12-08 18:16:37 -0500

    FF position as of 2024-12-08 reconciles with GMX (label != Ppsitive Funding Fees)

--- script/getCollateral.ts
+++ script/getCollateral.ts
@@ -6,8 +6,8 @@ import { usePositionsInfo } from '../src/gmx/domain/synthetics/positions'
 import { ethers } from 'ethers'
 
 import ff from 'ffpublic'
-import HARDCODED_GMX_POSITION from '../fixtures/20241201.json'
-//import HARDCODED_GMX_POSITION from '../fixtures/20241207_1637.json'
+//import HARDCODED_GMX_POSITION from '../fixtures/20241201.json'
+import HARDCODED_GMX_POSITION from '../fixtures/20241208_1710.json'
 import { writeFile } from 'fs/promises'
 
 function toSerializable(obj: any): any {
@@ -32,15 +32,19 @@ async function main() {
   try {
     const chainId = 42161
     const account = '0x8E1E8AA0deD409Aa6cA3E37E76239e3E3ff70BdF'
-    // const markets = await useMarketsInfo(chainId, account)
+    let positionsInfoData = HARDCODED_GMX_POSITION
+    const useHardcodedGmx = true
+    if (!useHardcodedGmx) {
+      const markets = await useMarketsInfo(chainId, account)
 
-    // const { positionsInfoData } = await usePositionsInfo(chainId, {
-    //   account: account,
-    //   marketsInfoData: markets.marketsInfoData,
-    //   tokensData: markets.tokensData,
-    //   showPnlInLeverage: false
-    // })
-    const positionsInfoData = HARDCODED_GMX_POSITION
+      const res = await usePositionsInfo(chainId, {
+        account: account,
+        marketsInfoData: markets.marketsInfoData,
+        tokensData: markets.tokensData,
+        showPnlInLeverage: false
+      })
+      positionsInfoData = res.positionsInfoData
+    }
 
     const serializablePositions = toSerializable(positionsInfoData)
 
@@ -65,7 +69,7 @@ async function main() {
         const externalSymbol = pos.marketInfo.name + ' ' + pos.collateralToken.symbol
         const interest = Number(pos.pendingClaimableFundingFeesUsd) / 10 ** 30
         const symbol = 'BTC.USD-PERP:CRYP'
-        const ffWireIn = ff.ffWireInSymbol({
+        const ffWireInCollateral = ff.ffWireInSymbol({
           dateStr: positionDate.toISOString(),
           symbol: pos.collateralToken.symbol === 'BTC' ? 'BTC.USD:CRYP' : 'USDT',
           amount: Number(pos.collateralAmount) / 10 ** pos.collateralToken.decimals,
@@ -75,9 +79,84 @@ async function main() {
           label: externalSymbol,
           assetType: 'CRYPTO'
         })
-        ffJson.accounts.push({ name: externalSymbol, transactions: [ffWireIn] })
+        const ffWireInFutures = ff.ffFuturesTrade({
+          amount: -Number(pos.sizeInTokens) / 10 ** pos.marketInfo.indexToken.decimals,
+          assetType: 'CRYPTO',
+          currency: 'USDT',
+          dateStr: positionDate.toISOString(),
+          description: 'Off market futures trade at initial upload',
+          instruction: 'SELL',
+          label: externalSymbol,
+          price: Number(pos.entryPrice) / 10 ** 30,
+          symbol: 'BTC.USD-PERP:CRYP',
+          symbolCurrency: 'USDT'
+        })
+        const ffWireOutPendingBorrowingFees = ff.ffWireOutCurrency(
+          {
+            dateStr: positionDate.toISOString(),
+            symbol: 'USDT',
+            netAmount: -Number(pos.pendingBorrowingFeesUsd) / 10 ** 30,
+            description: 'Borrowing Fees (Pending as of Initial Upload)',
+            currency: 'USDT',
+            symbolCurrency: 'USDT',
+            label: 'externalSymbol',
+            assetType: 'CRYPTO'
+          },
+          { forceEodUtc: false }
+        )
+        const ffWireOutClosingFee = ff.ffWireOutCurrency(
+          {
+            dateStr: positionDate.toISOString(),
+            symbol: 'USDT',
+            netAmount: -Number(pos.closingFeeUsd) / 10 ** 30,
+            description: 'Closing Fees (Pending as of Initial Upload)',
+            currency: 'USDT',
+            symbolCurrency: 'USDT',
+            label: 'Closing Fees',
+            assetType: 'CRYPTO'
+          },
+          { forceEodUtc: false }
+        )
+        const ffWireOutPendingFundingFees = ff.ffWireOutCurrency(
+          {
+            dateStr: positionDate.toISOString(),
+            symbol: 'USDT',
+            netAmount: -Number(pos.pendingFundingFeesUsd) / 10 ** 30,
+            description: 'Funding Fees (Pending as of Initial Upload)',
+            currency: 'USDT',
+            symbolCurrency: 'USDT',
+            label: 'Negative Funding Fees',
+            assetType: 'CRYPTO'
+          },
+          { forceEodUtc: false }
+        )
+        const ffWireInPendingClaimableFundingFees = ff.ffWireInCurrency(
+          {
+            dateStr: positionDate.toISOString(),
+            symbol: 'USDT',
+            netAmount: Number(pos.pendingClaimableFundingFeesUsd) / 10 ** 30,
+            description: 'Claimable Funding Fees (Pending as of Initial Upload)',
+            currency: 'USDT',
+            symbolCurrency: 'USDT',
+            label: 'Positive Funding Fees',
+            assetType: 'CRYPTO'
+          },
+          { forceEodUtc: false }
+        )
+
+        ffJson.accounts.push({
+          name: externalSymbol,
+          transactions: [
+            ffWireInCollateral,
+            ffWireInFutures,
+            ffWireOutPendingBorrowingFees,
+            ffWireOutClosingFee,
+            ffWireOutPendingFundingFees,
+            ffWireInPendingClaimableFundingFees
+          ]
+        })
         console.log('#################################')
-        console.log(ffWireIn)
+        console.log(ffWireInCollateral)
         console.log(ffJson)
         console.log('#################################')
       } else {
@@ -87,22 +166,24 @@ async function main() {
 
     // Write the ffJson object to a file
     await writeFile('ffJson.json', JSON.stringify(ffJson, null, 2), 'utf8')
-
-    const now = new Date()
-    const formattedDate = now
-      .toISOString()
-      .slice(0, 10)
-      .replace(/-/g, '')
-    const formattedTime = now
-      .toTimeString()
-      .slice(0, 5)
-      .replace(':', '')
-    await writeFile(
-      `fixtures/${formattedDate}_${formattedTime}.json`,
-      JSON.stringify(serializablePositions, null, 2),
-      'utf8'
-    )
     console.log('JSON has been successfully written to ffJson.json')
+
+    if (!useHardcodedGmx) {
+      const now = new Date()
+      const formattedDate = now
+        .toISOString()
+        .slice(0, 10)
+        .replace(/-/g, '')
+      const formattedTime = now
+        .toTimeString()
+        .slice(0, 5)
+        .replace(':', '')
+      await writeFile(
+        `fixtures/${formattedDate}_${formattedTime}.json`,
+        JSON.stringify(serializablePositions, null, 2),
+        'utf8'
+      )
+    }
 
     return
   } catch (error) {
